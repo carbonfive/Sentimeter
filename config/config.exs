be254d916cc59c8d2034586d6f59e2c6de97c8a3
hannahhoward <hannah@hannahhoward.net>
2019-05-07 13:04:30 -0700

    fix(bamboo): configure json library

--- config/config.exs
+++ config/config.exs
@@ -25,8 +25,9 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# Use Jason for JSON parsing in Phoenix
+# Use Jason for JSON parsing in Phoenix & Bamboo
 config :phoenix, :json_library, Jason
+config :bamboo, :json_library, Jason
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
