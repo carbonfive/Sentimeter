f2cd19f43a70a650d6df596db90a5e7367e82d0a
hannahhoward <hannah@hannahhoward.net>
2019-05-07 15:01:50 -0700

    build(prod): config prod settings

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :sentimeter, SentimeterWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: "sentimeter-test.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "c5-sentimeter.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
