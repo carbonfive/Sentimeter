4ed250a60aa8a2153f20ae97fd727e096080ce96
hannahhoward <hannah@hannahhoward.net>
2019-05-07 12:28:39 -0700

    fix(prod configs): use only subdomain

--- config/prod.exs
+++ config/prod.exs
@@ -10,6 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :sentimeter, SentimeterWeb.Endpoint,
+  load_from_system_env: true,
   url: [scheme: "https", host: "sentimeter-test.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
--- lib/sentimeter_web/endpoint.ex
+++ lib/sentimeter_web/endpoint.ex
@@ -47,4 +47,13 @@ defmodule SentimeterWeb.Endpoint do
     signing_salt: "4UIBeOW8"
 
   plug SentimeterWeb.Router
+
+  def init(_key, config) do
+    if config[:load_from_system_env] do
+      port = System.get_env("PORT") || raise "expected the PORT environment variable to be set"
+      {:ok, Keyword.put(config, :http, [:inet6, port: port])}
+    else
+      {:ok, config}
+    end
+  end
 end
